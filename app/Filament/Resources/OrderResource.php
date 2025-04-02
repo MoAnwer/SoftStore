a199109876ac3f1bcf85ed94fc55958360e348e2
mohamed <mhmdlyman362@gmail.com>
2025-04-02 08:04:36 +0200

    add badges to order status column & payment status column

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -204,23 +204,37 @@ class OrderResource extends Resource
                     ->money('SDG'),
                 TextColumn::make('payment_method')
                     ->searchable(),
+                TextColumn::make('status')
+                    ->badge()
+                    ->icon(fn($state) => match ($state) {
+                        'new' => 'heroicon-m-sparkles',
+                        'processing' => 'heroicon-m-arrow-path',
+                        'shipping' => 'heroicon-m-truck',
+                        'delivered' => 'heroicon-m-check-badge',
+                        'cancelled' => 'heroicon-m-x-circle'
+                    })
+                    ->color(fn($state) => match ($state) {
+                        'new' => 'info',
+                        'processing' => 'warning',
+                        'shipping' => 'primary',
+                        'delivered' => 'success',
+                        'cancelled' => 'danger'
+                    })
+                    ->searchable()
+                    ->sortable(),
                 TextColumn::make('payment_status')
+                    ->sortable()
+                    ->badge()
+                    ->color(fn($state) => match ($state) {
+                        'paid'     => 'success',
+                        'pending'  => 'warning',
+                        'failed'   => 'danger',
+                    })
                     ->searchable(),
                 TextColumn::make('currency')
                     ->searchable(),
                 TextColumn::make('shipping_method')
                     ->searchable(),
-                
-                SelectColumn::make('status')
-                    ->options([
-                        'new' => 'New',
-                        'processing' => 'Processing',
-                        'shipping' => 'Shipping',
-                        'delivered' => 'Delivered',
-                        'cancelled' => 'Cancelled'
-                    ])
-                    ->searchable()
-                    ->sortable(),
 
                 TextColumn::make('created_at')
                     ->dateTime()
