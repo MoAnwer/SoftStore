269b8bfec304a623c049076767a9e4f8ad7dca07
mohamed <mhmdlyman362@gmail.com>
2025-04-02 10:24:11 +0200

    Add badget for orders tabs filters

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -31,22 +31,32 @@ class ListOrders extends ListRecords
     {
         return [
             null => Tab::make('All'),
-            'New' => Tab::make('new')
+            'New' => Tab::make('New')
                     ->modifyQueryUsing(fn() => Order::query()->where('status', 'new'))
+                    ->badge(Order::query()->where('status', 'new')->count())
+                    ->badgeColor('info')
                     ->icon('heroicon-m-sparkles'),
 
             'Processing' => Tab::make('Processing')
                             ->query(fn($query) => $query->where('status', 'processing'))
+                            ->badge(Order::query()->where('status', 'processing')->count())
+                            ->badgeColor('warning')
                             ->icon('heroicon-m-arrow-path'),
 
             'Shipping' => Tab::make('Shipping')
                         ->query(fn($query) => $query->where('status', 'shipping'))
+                        ->badge(Order::query()->where('status', 'shipping')->count())
+                        ->badgeColor('primary')
                         ->icon('heroicon-m-truck'),
             'Delivered' => Tab::make('Delivered')
                         ->query(fn($query) => $query->where('status', 'delivered'))
+                        ->badge(Order::query()->where('status', 'delivered')->count())
+                        ->badgeColor('success')
                         ->icon('heroicon-m-check-badge'),
             'Cancelled' => Tab::make('Cancelled')
                         ->query(fn($query) => $query->where('status', 'cancelled'))
+                        ->badge(Order::query()->where('status', 'cancelled')->count())
+                        ->badgeColor('danger')
                         ->icon('heroicon-m-x-circle'),
         ];
     }
