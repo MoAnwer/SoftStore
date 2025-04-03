94ec3d173d1b0e22e2cc6a6e44481c3210c4c10a
mohamed <mhmdlyman362@gmail.com>
2025-04-03 08:10:59 +0200

    Install tailwindcss & livewire components

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-2xl text-red-300">Hello World</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/e', function (){
-    return event(new \App\Events\UserRegisterd(\App\Models\User::find(1)));
-});
+Route::get('/', HomePage::class);
